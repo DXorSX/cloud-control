7d7a18a8a4ef8b5dea35795cc0f2a6a47459587d
avpnusr <43516996+avpnusr@users.noreply.github.com>
2020-07-03 14:19:44 +0200

    streamlined and added multi-architecture support
    also changed the download url for hcloud, to always pull the latest image

--- prepenv/00-setup_controlnode.sh
+++ prepenv/00-setup_controlnode.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
-if [ "$OSTYPE" == "darwin19" ]; then
-	BIN=`command -v brew`
-	if [ -n "$BIN" ]; then
-		echo "OS is MacOS, installing hcloud via homebrew";
-		brew install hcloud;
-	else
-		echo "OS is MacOS, homebrew is need for autoinstall hcloud"
-	fi
-elif [ "$OSTYPE" == "linux-gnu" ]; then
-	BIN=`command -v wget`
-	if [ -n "$BIN" ]; then
-                echo "OS is Linux, installing hcloud";
-                mkdir ~/hcloud
-		cd ~/hcloud
-		wget https://github.com/hetznercloud/cli/releases/download/v1.16.2/hcloud-linux-amd64.tar.gz
-		tar -zxvf hcloud-linux-amd64.tar.gz
-        else
-                echo "OS is Linux, wget not found";
-        fi
+OSARCH=$(uname -m)
+
+case "$OSARCH" in
+x86_64)         PKGARCH=amd64 ;;
+armv6l)         PKGARCH=armv6 ;;
+armv7l)         PKGARCH=armv7 ;;
+aarch64)        PKGARCH=arm64 ;;
+i386|i686)      PKGARCH=386   ;;
+esac
+
+case "$OSTYPE" in
+darwin19)		PKGOS=macos		;;
+linux-gnu*)		PKGOS=linux 	;;
+FreeBSD)		PKGOS=freebsd 	;;
+esac
+
+if [ -n "$OSTYPE" ] && command -v wget $> /dev/null; then
+	echo "Running OS is ${PKGOS^^}, continuing to install hcloud";
+  	mkdir ~/hcloud
+	wget -c https://github.com/hetznercloud/cli/releases/latest/download/hcloud-$PKGOS-$PKGARCH.tar.gz -O - | tar -xzf - -C ~/hcloud hcloud
+	exit 0
 fi
+echo "ERROR: You are runninng an unsupported OS (only Linux, MacOS, FreeBSD are supported) or need to install the *wget* command"
+exit 1
